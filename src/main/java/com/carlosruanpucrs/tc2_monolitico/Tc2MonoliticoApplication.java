e1fbb475cd8b9967688f5d93fa1afe70446e85b3
Carlos Ruan <c.ruan@edu.pucrs.br>
2025-09-17 23:31:38 -0300

    projeto criado

---
+++ src/main/java/com/carlosruanpucrs/tc2_monolitico/Tc2MonoliticoApplication.java
@@ -0,0 +1,13 @@
+package com.carlosruanpucrs.tc2_monolitico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tc2MonoliticoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tc2MonoliticoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carlosruanpucrs/tc2_monolitico/Tc2MonoliticoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlosruanpucrs.tc2_monolitico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tc2MonoliticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
